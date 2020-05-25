365d3e44d37702fd57217b356a1e5344ba06ca5f
Shivanand Kamble <shivcollegeacc@gmail.com>
2020-05-25 22:58:11 +0530

    resume changes

---
+++ precache-manifest.15d3f3469fb2b26afe1333def635e62b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e76b51e1e078f133ca1c0f9ebda144c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92b2e421ff5566d3044a",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6f99435976017b0b8113",
+    "url": "/static/js/2.23c68dbf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.23c68dbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92b2e421ff5566d3044a",
+    "url": "/static/js/main.68a453df.chunk.js"
+  },
+  {
+    "revision": "9e81dc6d0717007efc7f",
+    "url": "/static/js/runtime-main.9734f21b.js"
+  }
+]);
